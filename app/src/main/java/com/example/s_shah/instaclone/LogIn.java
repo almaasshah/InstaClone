f9326162e9d3a3fcc876dccec721937e17e6f9e5
S-Shah <almaasshah>
2018-12-05 10:25:17 +0000

    Progress Dialog Added for Sign Up.

--- app/src/main/java/com/example/s_shah/instaclone/LogIn.java
+++ app/src/main/java/com/example/s_shah/instaclone/LogIn.java
@@ -7,8 +7,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class LogIn extends AppCompatActivity {
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+public class LogIn extends AppCompatActivity implements View.OnClickListener{
+
+
     private EditText loginEmail;
     private EditText loginPassWord;
     private Button lLogin;
@@ -19,19 +28,49 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        setTitle("Login");
 
         loginEmail = findViewById(R.id.loginEmail);
         loginPassWord = findViewById(R.id.loginPassWord);
         lLogin = findViewById(R.id.lLogin);
         lSignUp = findViewById(R.id.lSignUp);
 
-        lSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LogIn.this, SignUp.class);
-                startActivity(intent);
-            }
-        });
+        lLogin.setOnClickListener(this);
+        lSignUp.setOnClickListener(this);
+
+        if (ParseUser.getCurrentUser() != null){
+            ParseUser.getCurrentUser().logOut();
+        }
 
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.lSignUp:
+                Intent intent = new Intent(LogIn.this, SignUp.class);
+                startActivity(intent);
+                break;
+
+
+            case R.id.lLogin:
+                ParseUser.logInInBackground(loginEmail.getText().toString(), loginPassWord.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if(user != null && e == null){
+                            FancyToast.makeText(LogIn.this,
+                                    user.getUsername()+ " is Logged in.",
+                                    Toast.LENGTH_SHORT,FancyToast.SUCCESS,
+                                    true).show();
+                        }else{
+                            FancyToast.makeText(LogIn.this, e.getMessage(), Toast.LENGTH_LONG, FancyToast.ERROR, true).show();
+                        }
+
+                    }
+                });
+                break;
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/s_shah/instaclone/SignUp.java
+++ app/src/main/java/com/example/s_shah/instaclone/SignUp.java
@@ -1,14 +1,22 @@
 package com.example.s_shah.instaclone;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class SignUp extends AppCompatActivity {
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
+public class SignUp extends AppCompatActivity implements View.OnClickListener{
+//Ui Components
     private EditText emailAddress;
     private EditText userName;
     private EditText passWord;
@@ -19,6 +27,7 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        setTitle("Sign Up");
 
         emailAddress = findViewById(R.id.emailAddress);
         userName = findViewById(R.id.userName);
@@ -26,16 +35,49 @@ public class SignUp extends AppCompatActivity {
         sSignUp = findViewById(R.id.sSignUp);
         sLogIn = findViewById(R.id.sLogIn);
 
+        sLogIn.setOnClickListener(this);
+        sSignUp.setOnClickListener(this);
 
-        sLogIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        if (ParseUser.getCurrentUser() != null) {
+            ParseUser.getCurrentUser().logOut();
+        }
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.sLogIn:
                 Intent intent = new Intent(SignUp.this, LogIn.class);
                 startActivity(intent);
-            }
-        });
+                break;
+
+            case R.id.sSignUp:
+                final ParseUser appUser = new ParseUser();
+                appUser.setEmail(emailAddress.getText().toString());
+                appUser.setUsername(userName.getText().toString());
+                appUser.setPassword(passWord.getText().toString());
+
+                final ProgressDialog progressDialog = new ProgressDialog(this);
+                progressDialog.setMessage("Signing Up "+ userName.getText().toString());
+                progressDialog.show()
+                appUser.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e==null){
+                            FancyToast.makeText(SignUp.this,
+                                    appUser.getUsername() + " is Signed Up",
+                                    Toast.LENGTH_SHORT, FancyToast.SUCCESS,
+                                    true).show();
+                        }else{
+                            FancyToast.makeText(SignUp.this,
+                                    "There was an error: "+ e.getMessage(),
+                                    Toast.LENGTH_LONG, FancyToast.ERROR,
+                                    true).show();
+                        }
+                        progressDialog.dismiss();
+                        
+                    }
+                });
+                break;
+        }
     }
-
-
-
 }
